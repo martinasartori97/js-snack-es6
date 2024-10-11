b92a101e5ff2a6c36b12c6c1db602f9952d8dfb6
martinasartori97 <martinasartori9@gmail.com>
2024-10-11 14:49:02 +0200

    edit js

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("hello");
+
+
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore
+
+const biciDaCorsa = [
+    {
+        nome: "Bici A",
+        peso: 7.5,
+    },
+    {
+        nome: "Bici B",
+        peso: 6.8 
+    },
+    {
+        nome: "Bici C",
+        peso: 8.2 
+    },
+    {
+        nome: "Bici D",
+        peso: 7.0 
+    }
+];
+console.log(biciDaCorsa);
+
+
+
